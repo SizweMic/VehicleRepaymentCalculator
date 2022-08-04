49a4b4f86160d3ac61243fffcf3c6c3aff8bf76d
malum <malum@LAPTOP-OBQ6GA6K>
2022-08-04 09:51:21 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblDisplay = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblInterest = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtVehiclePrice = New System.Windows.Forms.TextBox()
+        Me.txtDeposit = New System.Windows.Forms.TextBox()
+        Me.cmdMonths = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 60)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Vehicle Price:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(39, 89)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Deposit:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(38, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Months:"
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.BackColor = System.Drawing.SystemColors.Window
+        Me.lblDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblDisplay.Location = New System.Drawing.Point(31, 156)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(298, 61)
+        Me.lblDisplay.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(222, 60)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Rands"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(222, 89)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 15)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Rands"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(222, 117)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(75, 15)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Interest Rate:"
+        '
+        'lblInterest
+        '
+        Me.lblInterest.BackColor = System.Drawing.SystemColors.Window
+        Me.lblInterest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblInterest.Location = New System.Drawing.Point(303, 110)
+        Me.lblInterest.Name = "lblInterest"
+        Me.lblInterest.Size = New System.Drawing.Size(25, 23)
+        Me.lblInterest.TabIndex = 7
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label8.Location = New System.Drawing.Point(334, 112)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(22, 20)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "%"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(31, 228)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(81, 31)
+        Me.btnCalculate.TabIndex = 9
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(130, 228)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(86, 32)
+        Me.btnClear.TabIndex = 10
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(247, 228)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(82, 32)
+        Me.btnExit.TabIndex = 11
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtVehiclePrice
+        '
+        Me.txtVehiclePrice.Location = New System.Drawing.Point(95, 52)
+        Me.txtVehiclePrice.Name = "txtVehiclePrice"
+        Me.txtVehiclePrice.Size = New System.Drawing.Size(121, 23)
+        Me.txtVehiclePrice.TabIndex = 12
+        '
+        'txtDeposit
+        '
+        Me.txtDeposit.Location = New System.Drawing.Point(95, 81)
+        Me.txtDeposit.Name = "txtDeposit"
+        Me.txtDeposit.Size = New System.Drawing.Size(121, 23)
+        Me.txtDeposit.TabIndex = 13
+        '
+        'cmdMonths
+        '
+        Me.cmdMonths.FormattingEnabled = True
+        Me.cmdMonths.Location = New System.Drawing.Point(95, 110)
+        Me.cmdMonths.Name = "cmdMonths"
+        Me.cmdMonths.Size = New System.Drawing.Size(121, 23)
+        Me.cmdMonths.TabIndex = 14
+        Me.cmdMonths.Text = "Select Months"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(360, 282)
+        Me.Controls.Add(Me.cmdMonths)
+        Me.Controls.Add(Me.txtDeposit)
+        Me.Controls.Add(Me.txtVehiclePrice)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.lblInterest)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lblDisplay)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblDisplay As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents lblInterest As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtVehiclePrice As TextBox
+    Friend WithEvents txtDeposit As TextBox
+    Friend WithEvents cmdMonths As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        cmdMonths.Items.Add(36)
+        cmdMonths.Items.Add(54)
+        cmdMonths.Items.Add(72)
+
+
+    End Sub
+
+    Public Function calcInstall(ByVal decVehiclePrice As Decimal, ByVal decDeposit As Decimal,
+                                ByVal Months As Integer, ByVal Interest As Decimal)
+
+        Dim MonthlyRepayment As Decimal
+        MonthlyRepayment = ((((decVehiclePrice - decDeposit) / Months) * Interest) _
+            + (decVehiclePrice - decDeposit) / Months)
+        Return "The monthly payment amount is: " & MonthlyRepayment.ToString("R###,###,###,##")
+
+    End Function
+
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim decVehiclePrice As Decimal = CDec(txtVehiclePrice.Text)
+        Dim decDeposit As Decimal = CDec(txtDeposit.Text)
+        Dim Months = cmdMonths.SelectedItem()
+        Dim interest = lblInterest.Text / 100
+
+        lblDisplay.Text = calcInstall(decVehiclePrice, decDeposit, Months, interest)
+
+
+    End Sub
+
+    Private Sub cmdMonths_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmdMonths.SelectedIndexChanged
+
+        If cmdMonths.SelectedIndex = 0 Then
+            lblInterest.Text = 10
+        ElseIf cmdMonths.SelectedIndex = 1 Then
+            lblInterest.Text = 13
+        ElseIf cmdMonths.SelectedIndex = 2 Then
+            lblInterest.Text = 15
+        End If
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        txtDeposit.Clear()
+        txtVehiclePrice.Clear()
+        lblDisplay.Text = ""
+        lblInterest.Text = ""
+        cmdMonths.Text = "Select months"
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        Application.Exit()
+
+    End Sub
+End Class
